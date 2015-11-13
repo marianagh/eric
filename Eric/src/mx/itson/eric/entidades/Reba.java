699c36f3a7b6fa739f6c17dba1c8bc7a3d5aa3ab
marianagh <marianagh@outlook.com>
2015-11-13 12:59:30 -0700

    Método Reba Terminado

--- Eric/src/mx/itson/eric/entidades/Reba.java
+++ Eric/src/mx/itson/eric/entidades/Reba.java
@@ -8,16 +8,191 @@ package mx.itson.eric.entidades;
 
 public class Reba {
 	
-	  
-    int carga;
-    int puntuacion;
-    int grupoA;
-    int cuello;
-    int piernas;
-    int tronco;
-    int grupoB;
-    int antebrazos;
-    int munecas;
-    int brazos;
+	private int id;
+    private int carga;
+    private int puntuacion;
+    private int grupoA;
+    private int cuello;
+    private int piernas;
+    private int tronco;
+    private int grupoB;
+    private int antebrazos;
+    private int munecas;
+    private int brazos;
+    private int actividadMuscular1;
+    private int actividadMuscular2;
+    private int actividadMuscular3;
+    
+    
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the actividadMuscular1
+	 */
+	public int getActividadMuscular1() {
+		return actividadMuscular1;
+	}
+	/**
+	 * @param actividadMuscular1 the actividadMuscular1 to set
+	 */
+	public void setActividadMuscular1(int actividadMuscular1) {
+		this.actividadMuscular1 = actividadMuscular1;
+	}
+	/**
+	 * @return the actividadMuscular2
+	 */
+	public int getActividadMuscular2() {
+		return actividadMuscular2;
+	}
+	/**
+	 * @param actividadMuscular2 the actividadMuscular2 to set
+	 */
+	public void setActividadMuscular2(int actividadMuscular2) {
+		this.actividadMuscular2 = actividadMuscular2;
+	}
+	/**
+	 * @return the actividadMuscular3
+	 */
+	public int getActividadMuscular3() {
+		return actividadMuscular3;
+	}
+	/**
+	 * @param actividadMuscular3 the actividadMuscular3 to set
+	 */
+	public void setActividadMuscular3(int actividadMuscular3) {
+		this.actividadMuscular3 = actividadMuscular3;
+	}
+	/**
+	 * @return the carga
+	 */
+	public int getCarga() {
+		return carga;
+	}
+	/**
+	 * @param carga the carga to set
+	 */
+	public void setCarga(int carga) {
+		this.carga = carga;
+	}
+	/**
+	 * @return the puntuacion
+	 */
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+	/**
+	 * @param puntuacion the puntuacion to set
+	 */
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+	/**
+	 * @return the grupoA
+	 */
+	public int getGrupoA() {
+		return grupoA;
+	}
+	/**
+	 * @param grupoA the grupoA to set
+	 */
+	public void setGrupoA(int grupoA) {
+		this.grupoA = grupoA;
+	}
+	/**
+	 * @return the cuello
+	 */
+	public int getCuello() {
+		return cuello;
+	}
+	/**
+	 * @param cuello the cuello to set
+	 */
+	public void setCuello(int cuello) {
+		this.cuello = cuello;
+	}
+	/**
+	 * @return the piernas
+	 */
+	public int getPiernas() {
+		return piernas;
+	}
+	/**
+	 * @param piernas the piernas to set
+	 */
+	public void setPiernas(int piernas) {
+		this.piernas = piernas;
+	}
+	/**
+	 * @return the tronco
+	 */
+	public int getTronco() {
+		return tronco;
+	}
+	/**
+	 * @param tronco the tronco to set
+	 */
+	public void setTronco(int tronco) {
+		this.tronco = tronco;
+	}
+	/**
+	 * @return the grupoB
+	 */
+	public int getGrupoB() {
+		return grupoB;
+	}
+	/**
+	 * @param grupoB the grupoB to set
+	 */
+	public void setGrupoB(int grupoB) {
+		this.grupoB = grupoB;
+	}
+	/**
+	 * @return the antebrazos
+	 */
+	public int getAntebrazos() {
+		return antebrazos;
+	}
+	/**
+	 * @param antebrazos the antebrazos to set
+	 */
+	public void setAntebrazos(int antebrazos) {
+		this.antebrazos = antebrazos;
+	}
+	/**
+	 * @return the munecas
+	 */
+	public int getMunecas() {
+		return munecas;
+	}
+	/**
+	 * @param munecas the munecas to set
+	 */
+	public void setMunecas(int munecas) {
+		this.munecas = munecas;
+	}
+	/**
+	 * @return the brazos
+	 */
+	public int getBrazos() {
+		return brazos;
+	}
+	/**
+	 * @param brazos the brazos to set
+	 */
+	public void setBrazos(int brazos) {
+		this.brazos = brazos;
+	}
+    
+    
 
 }
--- Eric/src/mx/itson/eric/implementacion/RebaImplementacion.java
+++ Eric/src/mx/itson/eric/implementacion/RebaImplementacion.java
@@ -9,7 +9,13 @@ import mx.itson.eric.interfaces.IReba;
  */
 
 public class RebaImplementacion implements IReba{
-
+	
+	/*
+	 * Calcula la puntuación de las piernas.
+	 * @param puntuacion Puntuación de la posición de las piernas. 
+	 * @param correccion Corrección de la posición de las piernas.
+	 * @return El valor de la puntuación de las piernas.
+	 */
 	@Override
 	public int calcularPiernas(int puntuacion, int correccion) {
 			int piernas = 0;
@@ -18,7 +24,12 @@ public class RebaImplementacion implements IReba{
 		    
 		    return piernas;
 	}
-
+	/*
+	 * Calcula la puntuación del cuello.
+	 * @param puntuacion Puntuación de la posición del cuello. 
+	 * @param correccion Corrección de la posición del cuello.
+	 * @return El valor de la puntuación del cuello.
+	 */
 	@Override
 	public int calcularCuello(int puntuacion, int correccion) {
 		int cuello = 0;
@@ -27,7 +38,12 @@ public class RebaImplementacion implements IReba{
         
         return cuello;
 	}
-
+	/*
+	 * Calcula la puntuación del tronco.
+	 * @param puntuacion Puntuación de la posición del tronco. 
+	 * @param correccion Corrección de la posición del tronco.
+	 * @return El valor de la puntuación del tronco.
+	 */
 	@Override
 	public int calcularTronco(int puntuacion, int correccion) {
 		int tronco = 0;
@@ -36,7 +52,13 @@ public class RebaImplementacion implements IReba{
 	    
 	    return tronco;
 	}
-	
+	/*
+	 * Calcula el valor del analisis de cuello piernas y tronco.
+	 * @param cuello Puntuación del cuello. 
+	 * @param piernas Puntuación de las piernas.
+	 * @param tronco Puntuación del tronco.
+	 * @return El valor del analisis en la tabla A.
+	 */
 	   public int grupoA(int cuello, int piernas, int tronco ){
 		    int a = 0;
 		    
@@ -172,7 +194,13 @@ public class RebaImplementacion implements IReba{
 		    }
 		    return a;
 		    }
-		    
+	   /*
+		 * Calcula el valor del analisis de brazos, antebrazos y muñecas.
+		 * @param cuello Puntuación de los brazos. 
+		 * @param piernas Puntuación de los antebrazos.
+		 * @param tronco Puntuación de las muñecas.
+		 * @return El valor del analisis en la tabla B.
+		 */
 		    public int gruboB (int antebrazo, int brazo, int muneca) {
 		         int grupoB = 0;
 		         
@@ -246,63 +274,166 @@ public class RebaImplementacion implements IReba{
 		         
 		        return grupoB;
 		    }
-
-			@Override
+		    /*
+			 * Calcula el valor de riesgo de actividad en base a los puntajes de grupo A y grupo B.
+			 * @param a Puntuación de la tabla A. 
+			 * @param piernas Puntuación de la tabla B.
+			 * @return El valor de riesgo según la tabla C.
+			 */			
 			public int tablaC(int a, int b) {
-				int c = 0;
-				switch (a){
 				
-				case 1: switch (b) {
-					case 1: c = 1; break;
-					case 2: c = 1; break;
-					case 3: c = 1; break;
-					case 4: c = 2; break;
-					case 5: c = 3; break;
-					case 6: c = 3; break;
-					case 7: c = 4; break;
-					case 8: c = 5; break;
-					case 9: c = 6; break;
-					case 10: c = 7; break;
-					case 11: c = 7; break;
-					case 12: c = 7; break;
-				} break;
+				int[][] tablaC = new int[12][12];
 				
-			case 2:  switch (b) {
-				case 1: c = 1; break;
-				case 2: c = 2; break;
-				case 3: c = 2; break;
-				case 4: c = 3; break;
-				case 5: c = 4; break;
-				case 6: c = 4; break;
-				case 7: c = 5; break;
-				case 8: c = 6; break;
-				case 9: c = 6; break;
-				case 10: c = 7; break;
-				case 11: c = 7; break;
-				case 12: c = 8; break;
-			} break;
-			
-			case 3:  switch (b) {
-			case 1: c = 2; break;
-			case 2: c = 3; break;
-			case 3: c = 3; break;
-			case 4: c = 3; break;
-			case 5: c = 4; break;
-			case 6: c = 5; break;
-			case 7: c = 6; break;
-			case 8: c = 6; break;
-			case 9: c = 7; break;
-			case 10: c = 7; break;
-			case 11: c = 8; break;
-			case 12: c = 8; break;
-		} break;
-		
-			
-				
-				
-				}
-				return c;
+				tablaC[0][0] = 0;
+				tablaC[1][1] = 1;
+				tablaC[1][2] = 1;
+				tablaC[1][3] = 1;
+				tablaC[1][4] = 2;
+				tablaC[1][5] = 3;
+				tablaC[1][6] = 3;
+				tablaC[1][7] = 4;
+				tablaC[1][8] = 5;
+				tablaC[1][9] = 6;
+				tablaC[1][10] =7;
+				tablaC[1][11] = 7;
+				tablaC[1][12] = 7;
+				tablaC[2][1] = 1;
+				tablaC[2][2] = 2;
+				tablaC[2][3] = 2;
+				tablaC[2][4] = 3;
+				tablaC[2][5] = 4;
+				tablaC[2][6] = 4;
+				tablaC[2][7] = 5;
+				tablaC[2][8] = 6;
+				tablaC[2][9] = 6;
+				tablaC[2][10] =7;
+				tablaC[2][11] = 7;
+				tablaC[2][12] = 8;
+				tablaC[3][1] = 2;
+				tablaC[3][2] = 3;
+				tablaC[3][3] = 3;
+				tablaC[3][4] = 3;
+				tablaC[3][5] = 4;
+				tablaC[3][6] = 5;
+				tablaC[3][7] = 6;
+				tablaC[3][8] = 6;
+				tablaC[3][9] = 7;
+				tablaC[3][10] = 7;
+				tablaC[3][11] = 8;
+				tablaC[3][12] = 8;
+				tablaC[4][1] = 3;
+				tablaC[4][2] = 4;
+				tablaC[4][3] = 4;
+				tablaC[4][4] = 4;
+				tablaC[4][5] = 5;
+				tablaC[4][6] = 6;
+				tablaC[4][7] = 7;
+				tablaC[4][8] = 8;
+				tablaC[4][9] = 8;
+				tablaC[4][10] = 9;
+				tablaC[4][11] = 9;
+				tablaC[4][12] = 9;
+				tablaC[5][1] = 4;
+				tablaC[5][2] = 4;
+				tablaC[5][3] = 4;
+				tablaC[5][4] = 5;
+				tablaC[5][5] = 6;
+				tablaC[5][6] = 7;
+				tablaC[5][7] = 8;
+				tablaC[5][8] = 8;
+				tablaC[5][9] = 9;
+				tablaC[5][10] = 9;
+				tablaC[5][11] = 9;
+				tablaC[5][12] = 9;
+				tablaC[6][1] = 4;
+				tablaC[6][2] = 4;
+				tablaC[6][3] = 4;
+				tablaC[6][4] = 5;
+				tablaC[6][5] = 6;
+				tablaC[6][6] = 7;
+				tablaC[6][7] = 8;
+				tablaC[6][8] = 8;
+				tablaC[6][9] = 9;
+				tablaC[6][10] = 9;
+				tablaC[6][11] = 9;
+				tablaC[6][12] = 9;
+				tablaC[7][1] = 7;
+				tablaC[7][2] = 7;
+				tablaC[7][3] = 7;
+				tablaC[7][4] = 8;
+				tablaC[7][5] = 9;
+				tablaC[7][6] = 9;
+				tablaC[7][7] = 9;
+				tablaC[7][8] = 10;
+				tablaC[7][9] = 10;
+				tablaC[7][10] = 11;
+				tablaC[7][11] = 11;
+				tablaC[7][12] = 11;
+				tablaC[8][1] = 8;
+				tablaC[8][2] = 8;
+				tablaC[8][3] = 8;
+				tablaC[8][4] = 9;
+				tablaC[8][5] = 10;
+				tablaC[8][6] = 10;
+				tablaC[8][7] = 10;
+				tablaC[8][8] = 10;
+				tablaC[8][9] = 10;
+				tablaC[8][10] = 11;
+				tablaC[8][11] = 11;
+				tablaC[8][12] = 11;
+				tablaC[9][1] = 9;
+				tablaC[9][2] = 9;
+				tablaC[9][3] = 9;
+				tablaC[9][4] = 10;
+				tablaC[9][5] = 10;
+				tablaC[9][6] = 10;
+				tablaC[9][7] = 11;
+				tablaC[9][8] = 11;
+				tablaC[9][9] = 11;
+				tablaC[9][10] = 12;
+				tablaC[9][11] = 12;
+				tablaC[9][12] = 12;
+				tablaC[10][1] = 10; 
+				tablaC[10][2] = 10;
+				tablaC[10][3] = 10;
+				tablaC[10][4] = 11;
+				tablaC[10][5] = 11;
+				tablaC[10][6] = 11;
+				tablaC[10][7] = 11;
+				tablaC[10][8] = 12;
+				tablaC[10][9] = 12;
+				tablaC[10][10] = 12;
+				tablaC[10][11] = 12;
+				tablaC[10][12] = 12;
+				tablaC[11][1] = 11;
+				tablaC[11][2] = 11;
+				tablaC[11][3] = 11;
+				tablaC[11][4] = 11;
+				tablaC[11][5] = 12;
+				tablaC[11][6] = 12;
+				tablaC[11][7] = 12;
+				tablaC[11][8] = 12;
+				tablaC[11][9] = 12;
+				tablaC[11][10] = 12;
+				tablaC[11][11] = 12;
+				tablaC[11][12] = 12;
+				tablaC[12][1] = 12;
+				tablaC[12][2] = 12;
+				tablaC[12][3] = 12;
+				tablaC[12][4] = 12;
+				tablaC[12][5] = 12;
+				tablaC[12][6] = 12;
+				tablaC[12][7] = 12;
+				tablaC[12][8] = 12;
+				tablaC[12][9] = 12;
+				tablaC[12][10] = 12;
+				tablaC[12][11] = 12;
+				tablaC[12][12] = 12;
+
+				return tablaC[a][b];				
+	
 			}
-		    
-		    
+
 }
+		    
+
